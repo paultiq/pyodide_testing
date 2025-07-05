82072122e47818a3f3516141f2b0e79c8f225783
paul@iqmo.com <paul@iqmo.com>
2025-07-05 13:00:16 -0400

    update runner temp run id

--- mre_5702/main.py
+++ mre_5702/main.py
@@ -21,7 +21,7 @@ def main():
     assert file.exists()
 
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     #options.add_argument("--no-sandbox")
     #options.add_argument("--disable-dev-shm-usage")
 
